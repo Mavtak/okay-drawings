3a354914a928dcf7d9159c1c4ec269c578c7e3b6
David McGrath <github.com@davidmcgrath.com>
2021-03-23 19:50:26 -0400

    unit test front end code

--- frontend/init.jsx
+++ frontend/init.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import document from './document.js';
 
-function init() {
+export default () => {
   let element = (
     <App />
   );
@@ -12,5 +13,3 @@ function init() {
 
   ReactDOM.render(element, root);
 }
-
-export default init;
