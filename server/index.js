23d8066edb6c75e37838f0ab9b0c03ca24abc7a8
David McGrath <github.com@davidmcgrath.com>
2021-03-23 11:32:44 -0400

    serve static content

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+app.use('/dist', express.static('dist'));
+
 app.listen(port, () => {
   console.log(`okay-drawings listening on http://localhost:${port}`);
 });
