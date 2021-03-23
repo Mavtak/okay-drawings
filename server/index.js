9c065053170d73e94ede637245aedaecb32274c4
David McGrath <github.com@davidmcgrath.com>
2021-03-23 11:57:03 -0400

    define catch-all route
    which eventually will serve the index.html file for the frontend

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const args = process.argv.slice(2);
 const app = express();
 const port = args[0] || 8080;
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
 app.use('/api', api);
 
 app.use('/dist', express.static('dist'));
+
+app.get('*', (req, res) => {
+  res.send('hello world');
+});
 
 app.listen(port, () => {
   console.log(`okay-drawings listening on http://localhost:${port}`);
