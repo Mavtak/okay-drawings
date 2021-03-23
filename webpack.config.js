71d0ba147346d5993cf6165998f39f689a29c358
David McGrath <github.com@davidmcgrath.com>
2021-03-23 12:24:35 -0400

    configure html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 
 export default {
@@ -10,4 +11,11 @@ export default {
     filename: '[name].js',
     path: path.resolve(process.cwd(), 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      publicPath: '/dist',
+      title: 'Okay Drawings',
+    }),
+  ],
 };
