9dfeea1258c639251e38f8310a10105fd8bf5be9
David McGrath <github.com@davidmcgrath.com>
2021-03-24 11:15:23 -0400

    implement login page

--- frontend/views/logIn/View.jsx
+++ frontend/views/logIn/View.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import userSession from '../../userSession.js';
 
-const View = () => (
-  <div>
-    login view
-  </div>
-);
+class View extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      username: '',
+    };
+  }
+
+  handleChangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handleLogIn = (event) => {
+    const {
+      username,
+    } = this.state;
+
+    event.preventDefault();
+
+    if (!username) {
+      return;
+    }
+
+    userSession.logIn({
+      username,
+    });
+  }
+
+  render = () => {
+    const {
+      username,
+    } = this.state;
+
+    return (
+      <form
+        onSubmit={this.handleLogIn}
+      >
+        <p>I trust you. enter the username that feels right.</p>
+        <p>
+          <input
+            onChange={this.handleChangeUsername}
+            type="text"
+            value={username}
+          />
+        </p>
+        <p>
+          <button
+            type="submit"
+          >
+            log in
+          </button>
+        </p>
+      </form>
+    );
+  }
+}
 
 export default View;
--- frontend/views/logIn/View.spec.jsx
+++ frontend/views/logIn/View.spec.jsx
@@ -2,7 +2,12 @@ import {
   shallow,
 } from 'enzyme';
 import React from 'react';
+import userSession from '../../userSession.js';
 import View from './View.jsx';
+
+jest.mock('../../userSession.js', () => ({
+  logIn: jest.fn(),
+}));
 
 describe('View', () => {
   let subject;
@@ -13,11 +18,75 @@ describe('View', () => {
     );
   });
 
-  it('renders as a div', () => {
-    expect(subject.type()).toBe('div');
+  it('has initial state', () => {
+    expect(subject.state()).toEqual({
+      username: '',
+    });
   });
 
-  it('contains some filler text', () => {
-    expect(subject.text()).toBe('login view');
+  describe('handleChangeUsername', () => {
+    it('sets the state', () => {
+      const event = {
+        target: {
+          value: 'new value',
+        },
+      };
+
+      subject.instance().handleChangeUsername(event);
+
+      expect(subject.state().username).toBe('new value');
+    });
+  });
+
+  describe('handleLogIn', () => {
+    let event;
+
+    beforeEach(() => {
+      event = {
+        preventDefault: jest.fn(),
+      };
+    });
+
+    it('prevents the browser form submitting', () => {
+      subject.instance().handleLogIn(event);
+
+      expect(event.preventDefault).toHaveBeenCalledWith();
+    });
+
+    describe('when the username is blank', () => {
+      beforeEach(() => {
+        subject.setState({
+          username: '',
+        });
+
+        subject.instance().handleLogIn(event);
+      });
+
+      it('does not call userSession.logIn', () => {
+        expect(userSession.logIn).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('when the username is set', () => {
+      beforeEach(() => {
+        subject.setState({
+          username: 'some.user',
+        });
+
+        subject.instance().handleLogIn(event);
+      });
+
+      it('calls userSession.logIn', () => {
+        expect(userSession.logIn).toHaveBeenCalledWith({
+          username: 'some.user',
+        });
+      });
+    });
+  });
+
+  describe('render', () => {
+    it('renders as a form', () => {
+      expect(subject.type()).toBe('form');
+    });
   });
 });
