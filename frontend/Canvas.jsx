9d5ecf46c563b59284b2e0c38d5db2d8a63b67d6
David McGrath <github.com@davidmcgrath.com>
2021-03-24 18:16:09 -0400

    update canvas to report drawing events

--- frontend/Canvas.jsx
+++ frontend/Canvas.jsx
@@ -2,12 +2,37 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Canvas = ({
+  onDraw,
   dimensionsPx,
   strokes,
   ...props
 }) => (
   <svg
     height={dimensionsPx.height}
+    onMouseDown={onDraw && (() => {
+      onDraw({
+        penDown: true,
+      });
+    })}
+    onMouseMove={onDraw && ((event) => {
+      const svg = event.target.tagName !==  'svg'
+        ? event.target.parentElement
+        : event.target;
+      const containerRects = svg.getClientRects()[0];
+      const point = {
+        x: event.clientX - containerRects.x,
+        y: event.clientY - containerRects.y
+      };
+
+      onDraw({
+        point,
+      });
+    })}
+    onMouseUp={onDraw && (() => {
+      onDraw({
+        penDown: false,
+      });
+    })}
     width={dimensionsPx.width}
     {...props}
   >
@@ -41,6 +66,7 @@ Canvas.propTypes = {
     height: PropTypes.number.isRequired,
     width: PropTypes.number.isRequired,
   }).isRequired,
+  onDraw: PropTypes.func,
   strokes: PropTypes.arrayOf(
     PropTypes.shape({
       end: coordinatesPropType.isRequired,
