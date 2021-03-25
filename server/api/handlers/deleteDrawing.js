a79b4adb6361eb5038f0a990489027daac334913
David McGrath <github.com@davidmcgrath.com>
2021-03-25 16:38:07 -0400

    authorize delete based on header

--- server/api/handlers/deleteDrawing.js
+++ server/api/handlers/deleteDrawing.js
@@ -2,12 +2,26 @@ import {
   validate as uuidValidate,
 } from 'uuid';
 import deleteDrawing from '../data/deleteDrawing.js';
+import readDrawing from '../data/readDrawing.js';
 
 export default async (req, res) => {
   const id = req.params.id;
+  const username = req.headers['x-username'];
 
   if (uuidValidate(id)) {
-    await deleteDrawing(id);
+    const drawing = await readDrawing(id);
+
+    if (drawing) {
+      if (drawing.user.username !== username) {
+        res
+          .status(403)
+          .end();
+
+        return;
+      }
+
+      await deleteDrawing(id);
+    }
   }
 
   res
