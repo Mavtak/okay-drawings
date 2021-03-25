df8308173133acb975f3c21e5ba597acf2c8b164
David McGrath <github.com@davidmcgrath.com>
2021-03-25 17:17:37 -0400

    authorize create route based on username header

--- server/api/handlers/createDrawing.js
+++ server/api/handlers/createDrawing.js
@@ -2,6 +2,15 @@ import createDrawing from '../data/createDrawing.js';
 
 export default async (req, res) => {
   const username = req.headers['x-username'];
+
+  if (!username) {
+    res
+      .status(401)
+      .end();
+
+    return;
+  }
+
   const drawing = {
     ...req.body,
     user: {
