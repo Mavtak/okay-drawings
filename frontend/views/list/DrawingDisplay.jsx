42d8e7e4633d251031820557d2d318ffd11a45fd
David McGrath <github.com@davidmcgrath.com>
2021-03-25 15:00:14 -0400

    link to View page from listing

--- frontend/views/list/DrawingDisplay.jsx
+++ frontend/views/list/DrawingDisplay.jsx
@@ -1,45 +1,56 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
 import Canvas from '../../Canvas.jsx';
 import MetadataOverlay from './MetadataOverlay.jsx';
 
 const DrawingDisplay = ({
   drawing,
 }) => (
-  <div
+  <Link
+    to={`/view/${encodeURIComponent(drawing.id)}`}
     style={{
-      display: 'inline-block',
-      position: 'relative',
-      border: '1px solid black',
-      margin: '10px',
+      color: 'black',
     }}
   >
-    <Canvas
-      drawing={drawing}
-      resize={{
-        height: '200px',
-        width: '200px',
-      }}
-    />
     <div
       style={{
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        display: 'inline-block',
+        position: 'relative',
+        border: '1px solid black',
+        margin: '10px',
       }}
     >
-      <MetadataOverlay
+      <Canvas
         drawing={drawing}
+        resize={{
+          height: '200px',
+          width: '200px',
+        }}
       />
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        }}
+      >
+        <MetadataOverlay
+          drawing={drawing}
+        />
+      </div>
     </div>
-  </div>
+  </Link>
 );
 
 DrawingDisplay.propTypes = {
   drawing: PropTypes.shape({
     dimensionsPx: Canvas.propTypes.dimensionsPx,
+    id: PropTypes.string.isRequired,
     strokes: Canvas.propTypes.strokes,
   }).isRequired,
 };
