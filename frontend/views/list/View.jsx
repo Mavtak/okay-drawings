89364b398c6f48769a9773ff9ac3aa4da4be0fc5
David McGrath <github.com@davidmcgrath.com>
2021-03-25 11:30:46 -0400

    implement most basic of listings

--- frontend/views/list/View.jsx
+++ frontend/views/list/View.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import api from '../../api.js';
+import Canvas from '../../Canvas.jsx';
 
-const View = () => (
-  <div>
-    list view
-  </div>
-);
+class View extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      drawings: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    await this.loadDrawings();
+  }
+
+  loadDrawings = async () => {
+    const drawings = await api.listDrawings();
+
+    this.setState({
+      drawings: drawings.results,
+    });
+  }
+
+  render = () => {
+    const {
+      drawings,
+    } = this.state;
+
+    return (
+      <div>
+        {
+          drawings.map((drawing, index) => (
+            <Canvas
+              key={index}
+              {...drawing}
+            />
+          ))
+        }
+      </div>
+    );
+  }
+
+}
 export default View;
