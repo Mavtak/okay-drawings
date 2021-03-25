7b1f158009f26997d3160593faa3745b6c1094da
David McGrath <github.com@davidmcgrath.com>
2021-03-25 16:32:11 -0400

    read username from header

--- server/api/handlers/createDrawing.js
+++ server/api/handlers/createDrawing.js
@@ -1,7 +1,13 @@
 import createDrawing from '../data/createDrawing.js';
 
 export default async (req, res) => {
-  const drawing = req.body;
+  const username = req.headers['x-username'];
+  const drawing = {
+    ...req.body,
+    user: {
+      username,
+    },
+  };
   const id = await createDrawing(drawing);
 
   res
